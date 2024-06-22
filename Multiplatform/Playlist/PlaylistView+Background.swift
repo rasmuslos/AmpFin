7a09108fe16d4b52765fa7632e587dbca6c8653f
Riccardo Uva <riccardo.ingegnere@icloud.com>
2024-06-22 14:46:58 +0200

    show playlist image cover from jellyfin instead of the blurred colors

--- Multiplatform/Playlist/PlaylistView+Background.swift
+++ Multiplatform/Playlist/PlaylistView+Background.swift
@@ -13,17 +13,42 @@ internal extension PlaylistView {
     struct Background: View {
         let playlist: Playlist
         
-        @State private var imageColors = ImageColors()
+        @State private var coverImage: UIImage? // playlist image on jellyfin
+        
+        func fetchData(from url: URL) async throws -> Data {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            return data
+        }
         
         var body: some View {
-            FluidGradient(blobs: [imageColors.background, imageColors.detail, imageColors.secondary, imageColors.primary], highlights: [], speed: 0.1, blur: 0.7)
-                .background(.tertiary)
-                .task {
-                    await imageColors.update(cover: playlist.cover)
-                }
+            if let coverImage = coverImage {
+                Image(uiImage: coverImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .edgesIgnoringSafeArea(.all)
+            } else {
+                Rectangle()
+                    .foregroundStyle(.tertiary)
+                    .onAppear {
+                        Task.detached {
+                            do {
+                                if let url = playlist.cover?.url,
+                                   let uiImage = UIImage(data: try await fetchData(from: url)) {
+                                    self.coverImage = uiImage
+                                }
+                            } catch {
+                                print("Error during the retreival of the playlist image:", error)
+                            }
+                        }
+                    }
+            }
         }
     }
-}
+
+
+    }
+
 
 #Preview {
     PlaylistView.Background.init(playlist: Playlist.fixture)
