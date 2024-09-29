af140f9a4488c7e555bac160e339df55c876cbc9
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2024-09-29 00:08:57 -0700

    Refactor JellyfinClient+QuickConnect.swift for improved compatibility and Quick Connect login support

--- AmpFinKit/Sources/AFNetwork/Methods/JellyfinClient+QuickConnect.swift
+++ AmpFinKit/Sources/AFNetwork/Methods/JellyfinClient+QuickConnect.swift
@@ -10,12 +10,20 @@ import Foundation
 
 public extension JellyfinClient {
     func checkIfQuickConnectIsAvailable() async throws -> Bool {
-        let response = try await request(ClientRequest<Bool>(path: "/QuickConnect/Enabled", method: "GET"))
-        
-        return response
+        if JellyfinClient.shared.supports(.legacyQuickConnectStatus)  {
+            let response = try await request(ClientRequest<String>(path: "/QuickConnect/Status", method: "GET"))
+            
+            return response == "Active"
+        } else if JellyfinClient.shared.supports(.quickConnect) {
+            let response = try await request(ClientRequest<Bool>(path: "/QuickConnect/Enabled", method: "GET"))
+            
+            return response
+        } else {
+            return false
+        }
     }
     
-    func initiateQuickConnect() async throws -> (String, String, String, String) {
+    func initiateQuickConnect() async throws -> (String, String) {
         var method = "POST"
         if JellyfinClient.shared.supports(.legacyQuickConnect) {
             method = "GET"
@@ -25,17 +33,15 @@ public extension JellyfinClient {
         
         return (
             response.Code,
-            response.DeviceId,
-            response.DeviceName,
             response.Secret
         )
     }
     
-    func verifyQuickConnect(secret: String) async throws -> Bool {
+    func verifyQuickConnect(secret: String) async throws -> (Bool, String?) {
         let response = try await request(ClientRequest<QuickConnectResponse>(path: "/QuickConnect/Connect", method: "GET", query:[
             URLQueryItem(name: "Secret", value: secret)
         ]))
         
-        return response.Authenticated
+        return (response.Authenticated, response.Authentication)
     }
 }
